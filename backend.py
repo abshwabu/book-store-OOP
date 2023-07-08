67adf26be3ed818e000691859e6ef6500fc98e4c
ashwabu <abdulazizredi90@gmail.com>
2023-07-08 11:13:11 +0300

    self

--- backend.py
+++ backend.py
@@ -1,40 +1,40 @@
 import sqlite3
 class Database:
-    def __init__(self):
+    def __init__(self, db):
         db = sqlite3.connect("books.db")
         curs=db.cursor()
         curs.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title text, author text,year INTEGER,isbn INTEGER)")
         db.commit()
         db.close()
 
-    def insert( title, author, year, isbn):
+    def insert(self, title, author, year, isbn):
         db = sqlite3.connect("books.db")
         curs = db.cursor()
         curs.execute("INSERT INTO book  VALUES(NULL,?,?,?,?)",(title,author,year,isbn))
         db.commit()
         db.close()
-    def search( title="", author="", year="", isbn=""):
+    def search(self, title="", author="", year="", isbn=""):
         db = sqlite3.connect("books.db")
         curs = db.cursor()
         curs.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?",(title,author,year,isbn))
         rows = curs.fetchall()
         db.close()
         return rows
-    def viewAll():
+    def viewAll(self):
         db = sqlite3.connect("books.db")
         curs = db.cursor()
         curs.execute("SELECT * FROM book")
         rows = curs.fetchall()
         db.close()
         return rows
-    def delete(id):
+    def delete(self,id):
         db = sqlite3.connect("books.db")
         curs = db.cursor()
         curs.execute("DELETE FROM book WHERE id=?",(id,))
         db.commit()
         db.close()
 
-    def update(id, title, author, year, isbn):
+    def update(self,id, title, author, year, isbn):
         db = sqlite3.connect("books.db")
         curs= db.cursor()
         curs.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?",(title,author,year,isbn,id))
